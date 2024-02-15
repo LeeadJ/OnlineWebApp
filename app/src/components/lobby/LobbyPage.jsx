af349164839a75d55686c6ffb70634446eee88a6
Leead Jacobowitz <leead123@gmail.com>
2024-02-15 10:10:34 +0200

    css styling

--- app/src/components/lobby/LobbyPage.jsx
+++ app/src/components/lobby/LobbyPage.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import CodeBlockList from './CodeBlockList'
+import './lobbyPage.css';
 
 const LobbyPage = props => {
   const handleCodeBlockSelect = (id) => {
     props.setId(id);
   }
   return (
-    <div>
-        <h1>Choose a code block</h1>
-        <p>Click a title to see the code:</p>
+    <div className="lobby-container">
+        <h1 className="lobby-title">Choose a code block</h1>
+        <p className="lobby-description">Click a title to see the code:</p>
         <CodeBlockList setId={handleCodeBlockSelect}/>
     </div>
   )
